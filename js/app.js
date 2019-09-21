9d70db2190202a443e9765f457e2f944a4de7543
Jessica Soares <jessicasoaresfa@gmail.com>
2019-09-21 13:17:21 -0300

    Setting up TO-DO List

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
-// Enemies our player must avoid
+////////////////////////////////////////////////
+//////////////// ENEMIES CLASS ////////////////
+//////////////////////////////////////////////
+
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    // proprieties
+      // x position
+      // y position
+
+      // sprite image for our enemies
+      this.sprite = 'images/enemy-bug.png';
 };
 
+// METHODS
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // if enemy has not reached the end of the board
+        // move forward
+        // increment x by speed * dt
+
+      // else
+        // reset start position
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,19 +37,56 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+////////////////////////////////////////////////
+//////////////// PLAYER CLASS /////////////////
+//////////////////////////////////////////////
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// HERO class
+
+  // constructor
+
+      // proprieties
+
+          // x position
+          // y position
+          // sprite image
+
+      // methods
+
+          // update the player's position
+              // checkCollisions
+                  // did the player collide?
+              // checkWin
+                  // did the player reach the water?
+
+          // render
+              // draw player sprite in current x and y position
+
+          // handle keyboard input
+              // the player's x and y position is updated according to the keyboard handleInput
+
+          // reset hero's position
+              // player's position is reset to x and y original start value
+
+////////////////////////////////////////////////
+///////////// INSTANTIATE OBJECTS /////////////
+//////////////////////////////////////////////
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+        // new player object
+        // init enemies array (allEnemies)
+          // for each new enemy, create and push object into the array (newEnemy)
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
